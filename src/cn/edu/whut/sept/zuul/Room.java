d4512763237caa1ee8c0a144a1f164e089917f4f
zhangshuo <2354944078@qq.com>
2021-06-30 16:42:30 +0800

    注释完成，未对代码进行修改

--- src/cn/edu/whut/sept/zuul/Room.java
+++ src/cn/edu/whut/sept/zuul/Room.java
@@ -15,8 +15,8 @@ public class Room
     private HashMap<String, Room> exits;
 
     /**
-     *
-     * @param description
+     *类的构造函数，创建房间并增加对房间的描述
+     * @param description 描述房间的属性，由系统自动生成
      */
     public Room(String description)
     {
@@ -25,9 +25,9 @@ public class Room
     }
 
     /**
-     *
-     * @param direction
-     * @param neighbor
+     * 将房间与出口的关系存入HashMap中
+     * @param direction 键值对的key值，表示对房间的描述
+     * @param neighbor 键值对的value值，表示房间的出口
      */
     public void setExit(String direction, Room neighbor)
     {
@@ -35,8 +35,8 @@ public class Room
     }
 
     /**
-     *
-     * @return
+     *获得房间的描述
+     * @return 返回的是该实例的desciption属性
      */
     public String getShortDescription()
     {
@@ -44,8 +44,8 @@ public class Room
     }
 
     /**
-     *
-     * @return
+     * 此函数用于在游戏过程中对所处位置的描述
+     * @return 返回的是“You are‘所在地点’（换行）‘所有出口’”的字符串
      */
     public String getLongDescription()
     {
@@ -53,8 +53,8 @@ public class Room
     }
 
     /**
-     *
-     * @return
+     * 此方法用于获取该房间的出口
+     * @return 返回的是包含该房间所有的出口的字符串
      */
     private String getExitString()
     {
@@ -67,9 +67,9 @@ public class Room
     }
 
     /**
-     *
-     * @param direction
-     * @return
+     * 此方法用于找到出口的方向
+     * @param direction 房间的属性
+     * @return 返回的是房间可以前进的方向
      */
     public Room getExit(String direction)
     {
