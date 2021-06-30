e1dd9042ccc29aef87811e7ef0521cac01900fc7
zhangshuo <2354944078@qq.com>
2021-06-30 22:05:27 +0800

    对已有的代码使用checkstyle进行了一系列修改

--- src/cn/edu/whut/sept/zuul/Main.java
+++ src/cn/edu/whut/sept/zuul/Main.java
@@ -3,9 +3,9 @@ package cn.edu.whut.sept.zuul;
 /**
  *启动游戏用的类，调用Game类创建game实例，并调用实例的play函数开始游戏.
  */
-class Main {
+public class  Main {
 
-    void main(final String[] args) {
+    public static void main(final String[] args) {
         Game game = new Game();
         game.play();
     }
